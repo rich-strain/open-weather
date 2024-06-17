be52b2913b5bbb49e0a508ac2d9cd03c9ee90f49
Rich <richardstrain@gmail.com>
2024-06-17 16:53:34 -0400

    OpenWeather search by city Working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
 const APIKey = 'd036f25ab5a27eb890eb96c7ec17e827';
 const geoUrl = `http://api.openweathermap.org/geo/1.0/direct`;
-const lat = '27.2939333';
-const lon = '-80.3503283';
-//const dataUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+const searchFormEl = document.querySelector('#searchForm');
+const cityInputEl = document.querySelector('#cityInput');
 
-//q=Miami&limit=5&appid=d036f25ab5a27eb890eb96c7ec17e827?
-//q=Port%20Saint%20Lucie&limit=5&appid=d036f25ab5a27eb890eb96c7ec17e827
+// const lat = '27.2939333';
+// const lon = '-80.3503283';
 
+// build the URL for the city fetch request
 const prepCityUrl = function (cityName) {
-  console.log(`${geoUrl}?q=${cityName}&limit=5&appid=${APIKey}`);
+  //console.log(`${geoUrl}?q=${cityName}&limit=5&appid=${APIKey}`);
   return `${geoUrl}?q=${cityName}&limit=5&appid=${APIKey}`;
+};
+// build the URL for the forecast fetch request
+const prepForecastUrl = function (lat, lon) {
+  return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 };
 
 // Search City To Find Latitude and Longitude
 const searchCity = function (cityName) {
-  console.log('City Name: ', cityName);
+  console.log('searchCity: ', cityName);
   fetch(prepCityUrl(cityName))
     .then((response) => {
       if (!response.ok) {
@@ -23,10 +27,14 @@ const searchCity = function (cityName) {
       return response.json();
     })
     .then((data) => {
-      if (data) {
-        console.log(data);
-        console.log(`Lat : ${data[0].lat} , Lon: ${data[0].lon}`);
+      if (data && data.length > 0) {
+        const city = data[0];
+        console.log(city.name);
+        console.log(city.lat);
+        console.log(city.lon);
+        return city;
       }
+      return response.json();
     })
     .catch((error) => {
       console.error('Fetch error:', error);
@@ -36,60 +44,12 @@ const searchCity = function (cityName) {
 const getForecast = (event) => {
   event.preventDefault();
   const cityInput = cityInputEl.value.trim();
+  console.log('City Input: ', cityInput);
 
   if (cityInput) {
-    searchCity(cityInput);
-
-    //Possibly Clear Weather Card Elements txt values?
-    // card elements here
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter a city to get your weather forecast!');
+    const cityData = searchCity(cityInput);
+    console.log('City Data: ', cityData);
   }
 };
 
-$(document).ready(function () {});
-
-const staticCity = 'Port%20Saint%20Lucie';
-searchCity(staticCity);
-
-// Get Weather Forecast
-// getWeatherForecast(forecastURL)
-//   .then((data) => {})
-//   .catch((error) => {});
-// searchCity(cityURL)
-// .then((data) => {
-//     if (data && data.length > 0) {
-//     // Ensure data is not empty
-//     const city = data[0]; // Assuming we want the first result
-//     console.log(city.name);
-//     console.log(city.lat);
-//     console.log(city.lon);
-//     } else {
-//     console.log('No data found for the requested city.');
-//     }
-// })
-// .catch((error) => {
-//     console.error('Error in searchCity:', error);
-// });
-
-//const url = `https://api.openweathermap.org/data/2.5/forecast?lat=27&lon=-80&appid=${APIKey}`;
-
-//   let city = weather.name;
-//   let temp = weather.main.temp;
-//   let highTemp = weather.main.temp_max;
-//   let lowTemp = weather.main.temp_min;
-//   let humidity = weather.main.humidity;
-//   let windSpeed = weather.wind.speed;
-//   let uvIndex = weather.main.uvi;
-//   let pressure = weather.main.pressure;
-
-//   // assign text content to the elements
-//   document.getElementById('city').textContent = city;
-//   document.getElementById('temp').textContent = temp;
-//   document.getElementById('highTemp').textContent = highTemp;
-//   document.getElementById('lowTemp').textContent = lowTemp;
-//   document.getElementById('humidity').textContent = humidity;
-//   document.getElementById('windSpeed').textContent = windSpeed;
-//   document.getElementById('uvIndex').textContent = uvIndex;
-//   document.getElementById('pressure').textContent = pressure;
+searchFormEl.addEventListener('submit', getForecast);
