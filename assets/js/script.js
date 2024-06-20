82a8e2930ea9e1e794814aa42407469d1e8b4d62
Rich <richardstrain@gmail.com>
2024-06-20 11:33:58 -0400

    Updated Script Functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 const APIKey = 'd036f25ab5a27eb890eb96c7ec17e827';
 const geoUrl = `http://api.openweathermap.org/geo/1.0/direct`;
-const searchFormEl = document.querySelector('#searchForm');
 const cityInputEl = document.querySelector('#cityInput');
-
-// const lat = '27.2939333';
-// const lon = '-80.3503283';
 
 // build the URL for the city fetch request
 const prepCityUrl = function (cityName) {
@@ -16,40 +12,64 @@ const prepForecastUrl = function (lat, lon) {
   return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 };
 
-// Search City To Find Latitude and Longitude
-const searchCity = function (cityName) {
-  console.log('searchCity: ', cityName);
-  fetch(prepCityUrl(cityName))
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data && data.length > 0) {
-        const city = data[0];
-        console.log(city.name);
-        console.log(city.lat);
-        console.log(city.lon);
-        return city;
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.error('Fetch error:', error);
-    });
+const cityAlert = (cityName) => {
+  console.log('City Alert Function Running');
+  // Display Alert
+  const alertHtml = ` 
+     <div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>${cityName}</strong> could not be found! Please try searching a different city.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
+  $('#alert-placeholder').html(alertHtml);
 };
 
-const getForecast = (event) => {
-  event.preventDefault();
-  const cityInput = cityInputEl.value.trim();
-  console.log('City Input: ', cityInput);
-
-  if (cityInput) {
-    const cityData = searchCity(cityInput);
-    console.log('City Data: ', cityData);
+// Search City To Find Latitude and Longitude
+const searchCity = async (cityName) => {
+  try {
+    const response = await fetch(prepCityUrl(cityName));
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data && data.length > 0) {
+      const city = data[0];
+      return city;
+    } else {
+      console.log('No City Found');
+      return null;
+    }
+    //return response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
   }
 };
 
-searchFormEl.addEventListener('submit', getForecast);
+// Display Forecast Found By Latitude and Longitude
+const getForecast = async (lat, lon) => {
+  try {
+    console.log('fetchForecast For Lat: ', lat, ' Lon: ', lon);
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+};
+
+// Handle Form Submission
+const handleSubmit = (event) => {
+  console.log('Handle Submit Funciton Running');
+  event.preventDefault();
+  const cityInput = cityInputEl.value.trim();
+  console.log('City Input: ', cityInput);
+  if (cityInput) {
+    searchCity(cityInput).then((cityData) => {
+      if (cityData) {
+        console.log('City Data: ', cityData);
+      } else {
+        console.log('Trigger City Alert');
+        const alert = cityAlert(cityInput);
+      }
+      //getForecast(cityData.lat, cityData.lon);
+    });
+  }
+};
+
+$(document).ready(function () {
+  // add event listeners
+  $('#searchBtn').on('click', handleSubmit);
+});
